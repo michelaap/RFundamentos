6e90a4757c8f4649a0b3d68d65a94cb7037b5d7c
Michel A. A. Pereira <michel.augusto@pg.inatel.br>
2020-07-21 21:19:17 -0300

    fundamentos do RStudio

---
+++ Parte1/01-RStudio.R
@@ -0,0 +1,67 @@
+# Explorando o RStudio
+
+# Nome dos Contributors
+contributors()
+
+
+# Licença
+license()
+
+
+# Configurando o diretório de trabalho
+getwd()
+#setwd("path")
+
+
+# Informações sobre a sessão
+sessionInfo()
+
+
+# Imprimir na tela
+print('R - Uma das principais ferramentas do Cientista de Dados')
+
+
+# Criar gráficos
+plot(1:30)
+
+
+# Imprimir na tela
+print('R - Uma das principais ferramentas do Cientista de Dados')
+
+
+# Instalar pacotes
+install.packages('randomForest')
+install.packages('ggplot2')
+install.packages("dplyr")
+
+
+# Carregar o pacote
+library(ggplot2)
+
+
+# Descarregar o pacote
+detach(package:ggplot2)
+
+
+
+# Visualizando o diretório de trabalho
+getwd()
+
+
+# Se souber o nome da função
+help(mean)
+?mean
+
+
+# Se não souber o nome da função
+help.search('randomForest')
+help.search('matplot')
+??matplot
+RSiteSearch('matplot')
+example('matplot')
+
+
+# Sair
+q()
+
+
