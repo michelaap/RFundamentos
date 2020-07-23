6cb3dd4c9f963ba2542a7fe0c11c80a05448bd8a
Michel A. A. Pereira <michel.augusto@pg.inatel.br>
2020-07-22 21:04:10 -0300

    operadores

---
+++ Parte1/02-Operadores.R
@@ -0,0 +1,57 @@
+## Operadores Básicos, Relacionais e Lógicos em R
+
+# Operadores Básicos
+
+# Soma
+4 + 4
+
+# Subtração
+4 - 4
+
+# Multiplicação
+4 * 4
+
+# Divisão
+4 / 4
+
+# Potência
+4^2
+4**2
+
+# Módulo
+14 %% 3 
+
+
+# Operadores Relacionais
+
+# Atribuindo variáveis
+a = 7
+b = 5
+
+# Operadores
+a > 8
+a < 8
+a <= 8
+a >= 8
+a == 8
+a != 8
+
+
+# Operadores lógicos
+
+# And
+(a==8) & (b==6)
+(a==7) & (b==5)
+(a==8) & (b==5)
+
+# Or
+(a==8) | (b>5)
+(a==8) | (b>=5)
+
+# Not
+a > 8
+print(!a > 8)
+
+
+
+
